8934e992ee59f467340b386b3434e4903b5a9bf9
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-03-03 00:14:12 +0600

    ADD telegram api notification

--- wazzup-web.sh
+++ wazzup-web.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+TOKEN=
+CHAT_ID=
+API=https://api.telegram.org/bot$TOKEN/sendMessage
+MESSAGE="SERVER IS DOWN!"
 
 if curl -s --head --request GET <URL> | grep '200' > /dev/null; then 
      echo "Server is UP"
 else 
+     curl -s -X POST $API -d chat_id=$CHAT_ID -d text="$MESSAGE"
      echo "Server is DOWN"
 fi
