8cd4c602e68022d472fd26c435d563061691ba05
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-03-03 00:22:54 +0600

    ADD available status code and message head

--- wazzup-web.sh
+++ wazzup-web.sh
@@ -4,12 +4,14 @@ CHAT_ID=
 API=https://api.telegram.org/bot$TOKEN/sendMessage
 
 SERVER=
+MESSAGE_HEAD='SERVER IS DOWN!'
+AVAILABLE_STATUS_CODE='200|301'
 
 STATUS=`wget --spider -S $SERVER 2>&1 | grep "HTTP/" | awk '{print $2,$3,$4}'`
 DATE=`date +"%m.%d.%Y"`
 TIME=`date +"%H:%M:%S"`
 
-MESSAGE="SERVER IS DOWN!
+MESSAGE="${MESSAGE_HEAD}
 ----------------------------------------------------
 Date   :    ${DATE}
 Time   :    ${TIME}
@@ -18,7 +20,7 @@ Server :    ${SERVER}
 Status :    ${STATUS}
 ----------------------------------------------------"
 
-if curl -s --head --request GET $SERVER | grep '200' > /dev/null; then 
+if curl -s --head --request GET $SERVER | grep -E $AVAILABLE_STATUS_CODE > /dev/null; then 
      echo "Server is UP"
 else 
      curl -s -X POST $API -d chat_id=$CHAT_ID -d text="$MESSAGE"
